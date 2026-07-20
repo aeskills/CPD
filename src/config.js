6b4c3c8db6fdc696ee57be373d1eef72c062d3d5
aeskills <aeskills@example.com>
2026-07-20 16:46:49 +0530

    Update Google Apps Script URL for new Gmail account

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Google Sheets Apps Script Web App URL
 // Deploy the provided Apps Script in your Google Sheet and paste the Web App Executable URL below.
 // Example: "https://script.google.com/macros/s/AKfycbwX.../exec"
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwHwgRqmWgs6nJG314iFLBIIp9Ku05fPKKg-vHTXS7XQGmp_NWFtLGnP0aVa3fJBGi5/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbykVRwuZCeqr2_W1eB56RHtAD-6KiVLo9fBeqt9HUfFcvsyt8LS9xjJctrEbAJoy_NOwQ/exec";
 
